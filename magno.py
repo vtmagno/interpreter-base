4cba325ab47879514dcf22a45e30726276659595
vtmagno <vcmagno@up.edu.ph>
2018-11-11 16:44:45 +0800

    updated magno.py

--- magno.py
+++ magno.py
@@ -3,3 +3,38 @@
 
 # import all necessary imports
 from sys import *
+
+# ******************************************************** GLOBAL VARIABLES ********************************************************
+listOfTokens = [] # this list contains the parsed characters and represents them as whole strings
+listOfNumStack = [] # this list is for determining if the parsed tokens are expressions
+variableDictionary = {} # this dictionary will contain variables and their values
+
+# ******************************************************** open_file() METHOD ********************************************************
+# This method will open the file to be interpreted.
+
+def open_file(fileData):
+    fileData = open(ipolFile, "r").read()
+    fileData += "<EOF>"
+    return fileData
+
+    # fileData variable contains the contents of the ipolFile which was given
+    # by the user. It is set to READ ONLY. <EOF> is added to the end of the file
+    # to signify that there are no more lines to interpret.
+
+# ******************************************************** PROGRAM PROPER ********************************************************
+# This is the program proper. Its only objective is to ask the user for the name and extension of
+# the file to be interpreted. It will only accept files ending in ".ipol".
+
+while True: 
+
+    ipolFile = input("Please enter the file to be interpreted: ")
+
+    if ipolFile.endswith(".ipol"):
+        run_file()
+        break
+    else:
+        print("Please enter the file to be interpreted. Must be in .ipol extension.")
+
+# ipolFile variable contains the name and extension of the file the user wants to interpret. Once
+# the program receives the correct type of file, it will run the run_file() method. Else, the 
+# program will keep asking the user to input an .ipol file.
